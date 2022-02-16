bac97e1362ef869c1f1baf5682396339ea1a6710
Taylor Badraun <tbadraun@cnm.edu>
2022-02-15 18:04:45 -0600

    changed the contact form to be able to refresh, added 1st Trimester content

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -40,7 +40,7 @@ $(document).ready(function (){
                     $('#output-area').css('display','')
                     $('#output-area').html(ajaxOutput)
 
-                    if ($('.alert-success') >= 1){
+                    if ($('.alert-success' >= 1)){
                         $('#contact')[0].reset()
                     }
 
